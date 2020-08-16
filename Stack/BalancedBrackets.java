7d8ba376c6967c1cad0b6e93aeb18bbed23e8736
Rohan105 <54831424+rohan01101999@users.noreply.github.com>
2020-08-16 21:10:24 +0530

    Add files via upload

---
+++ Stack/BalancedBrackets.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+
+        System.out.println(balance(str));
+
+
+    }
+    public static boolean balance(String str) {
+        Stack < Character > st = new Stack < > ();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else if (ch == ')') {
+                if (st.size() == 0 || st.peek() != '(') {
+                    return false;
+                } else {
+                    st.pop();
+                }
+            } else if (ch == '}') {
+                if (st.size() == 0 || st.peek() != '{') {
+                    return false;
+                } else {
+                    st.pop();
+                }
+
+            } else if (ch == ']') {
+                if (st.size() == 0 || st.peek() != '[') {
+                    return false;
+                } else {
+                    st.pop();
+                }
+
+            }
+        }
+        return st.size() == 0;
+
+    }
+
+}
